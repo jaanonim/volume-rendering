192ef9145da64d5eaa26c50e339e6ea9ca2d715a
jaanonim <mat8mro@gmail.com>
2025-10-08 10:47:31 +0200

    webgl ctx setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function main() {
+  const canvas = document.getElementById("root") as HTMLCanvasElement;
+  const gl = canvas.getContext("webgl2");
+  if (gl === null) {
+    alert("Cannot get webgl2 context");
+    return;
+  }
+  gl.clearColor(1.0, 1.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+main();
